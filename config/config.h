4ba6dc5535cf6859558ff37d668ed13ca87c64c0
VicoAerts <vico.aerts@gmail.com>
2025-11-16 18:14:46 +0100

    config template

---
+++ config/config.h
@@ -0,0 +1,27 @@
+//
+// Created by gebruiker on 16/11/2025.
+//
+
+#pragma once
+
+namespace config {
+
+// --- Window / rendering ---
+// constexpr unsigned int window_width = 1920;
+// constexpr unsigned int window_height = 1080;
+// constexpr unsigned int max_fps = 60;
+//
+// // --- World / camera ---
+// constexpr float world_min_x = -1.0f;
+// constexpr float world_max_x = 1.0f;
+// constexpr float world_min_y = -1.0f;
+// constexpr float world_max_y = 1.0f;
+
+// --- Gameplay ---
+//  constexpr float pacman_speed = ;
+//  constexpr float ghost_base_speed = ;
+//  constexpr float fear_duration_s = ;
+
+// --- highscore files---
+
+} // namespace config
