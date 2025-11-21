ac8ba18cb2ca22073f254fd8bad3f3fdbe501d3c
VicoAerts <vico.aerts@gmail.com>
2025-11-21 12:53:51 +0100

    update& cleanup levelstate

--- src/states/LevelState.cpp
+++ src/states/LevelState.cpp
@@ -8,16 +8,17 @@
 
 namespace view::state {
 LevelState::LevelState(StateManager& stateManager)
-    : State(stateManager), m_factory(stateManager.getWindow()),
-      m_camera(Camera(stateManager.getWindow().getSize().x, stateManager.getWindow().getSize().y)) {
+    : State(stateManager), m_camera(Camera(stateManager.getWindow().getSize().x, stateManager.getWindow().getSize().y)),
+      m_factory(stateManager.getWindow()) {
     // 1. load maze from file
-    // 2. make world
-    // 3. make camera
-    // 4. create factory
-
     GridMap map;
     map.loadMazeFromFile("Maze.txt");
+    // 2. make world
     m_world = std::make_unique<model::World>(map, m_factory);
+    // 3. set camera grid size
+    m_camera.setGridSize(map.getHeight(), map.getWidth());
+    // 4. get views to render later
+    // m_entityViews = m_factory.getviews
 }
 void LevelState::handleEvents(const sf::Event& event) {}
 void LevelState::update(const double deltaTime) {}
