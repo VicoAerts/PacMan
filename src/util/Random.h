5a14c7393baa229923e9aefe407730535dd3952a
VicoAerts <vico.aerts@gmail.com>
2025-12-04 23:43:17 +0100

    random implementation

--- src/util/Random.h
+++ src/util/Random.h
@@ -1,10 +1,25 @@
-//
-// Created by gebruiker on 18/11/2025.
-//
-
+/**
+ *@file Random.h
+ *@brief Declaration of the Random class.
+ *
+ */
 #ifndef PACMAN_RANDOM_H
 #define PACMAN_RANDOM_H
+#include <random>
 
-class Random {};
+namespace util {
+class Random {
+public:
+    Random() : rng(std::random_device{}()) {}
+    /** Generate a random integer in the range [min, max] */
+    int randomInt(int min, int max) {
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(rng);
+    }
+
+private:
+    std::mt19937 rng;
+};
+} // namespace util
 
 #endif // PACMAN_RANDOM_H
